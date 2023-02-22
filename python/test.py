93526abdf445c32aeb0d8c5d12d6cf791bc1c464
blue <bluexh@gmail.com>
2023-02-22 16:55:27 +0900

    second commit

--- python/test.py
+++ python/test.py
@@ -1,4 +1,4 @@
 multifly = 19									# 곱하기에 사용할 변수를 선언
 
-for i in range(1, 20)	:						# 입력해야 할 범위를 지정
+for i in range(1, 30)	:						# 입력해야 할 범위를 지정
 	print(multifly, " ", "*", i, "=", multifly * i)	
